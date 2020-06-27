c27c7bdb3c70668f74a9ea813ea2f7c0ff744e44
Marek Gogol <9mike66@gmail.com>
2020-06-27 12:53:55 +0200

    added header token authentification

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 global.__basedir = __dirname;
 
 //Bootstrap .env files
-const dotenv = require('dotenv');
+const dotenv = require('dotenv'),
+     axios = require('axios');
 
 //Setup dotenv
 dotenv.config();
+
+//Setup axios token
+axios.defaults.headers.common = {
+    'APP-AUTH-TOKEN': process.env.APP_TOKEN,
+};
 
 const mksoft = require('./src/mksoft.js');
 
